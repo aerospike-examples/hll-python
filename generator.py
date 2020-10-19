6ca7f78a06190e27f0d4f3af3a20cf0e2c4b21ea
Andre Chang <achang@aerospike.com>
2020-10-19 11:12:12 -0700

    PEP8 by black

--- generator.py
+++ generator.py
@@ -1,70 +1,131 @@
 import random
 
 LOCATION = [
-	['vancouver', 'canada',],
-	['vancouver', 'washington',],
-	['mountain_view', 'california',],
-	['seattle', 'washington',],
-	['dublin', 'ireland',],
-	['dublin', 'california',],
+    [
+        "vancouver",
+        "canada",
+    ],
+    [
+        "vancouver",
+        "washington",
+    ],
+    [
+        "mountain_view",
+        "california",
+    ],
+    [
+        "seattle",
+        "washington",
+    ],
+    [
+        "dublin",
+        "ireland",
+    ],
+    [
+        "dublin",
+        "california",
+    ],
 ]
 
 TAGS = [
-	# search terms
-	['iceland', 'elves', 'waterfalls',],
-	['hiking', 'camping', 'waterfalls',],
-	['hobbits', 'elves', 'fiction', 'fantasy',],
-	['tesla', 'mercedes', 'bmw', 'toyota',],
-	['tesla', 'faraday', 'newton',],
-	['database', 'aerospike',],
-	['rocket', 'aerospike',],
-	# hobbies
-	['tennis',],
-	['kniting',],
-	['baking',],
-	['fishing',],
-	# owned cars
-	['tesla',],
-	['mercedes',],
-	['bmw',],
-	['toyota',],
+    # search terms
+    [
+        "iceland",
+        "elves",
+        "waterfalls",
+    ],
+    [
+        "hiking",
+        "camping",
+        "waterfalls",
+    ],
+    [
+        "hobbits",
+        "elves",
+        "fiction",
+        "fantasy",
+    ],
+    [
+        "tesla",
+        "mercedes",
+        "bmw",
+        "toyota",
+    ],
+    [
+        "tesla",
+        "faraday",
+        "newton",
+    ],
+    [
+        "database",
+        "aerospike",
+    ],
+    [
+        "rocket",
+        "aerospike",
+    ],
+    # hobbies
+    [
+        "tennis",
+    ],
+    [
+        "kniting",
+    ],
+    [
+        "baking",
+    ],
+    [
+        "fishing",
+    ],
+    # owned cars
+    [
+        "tesla",
+    ],
+    [
+        "mercedes",
+    ],
+    [
+        "bmw",
+    ],
+    [
+        "toyota",
+    ],
 ]
 
 
 def get_index(fraction, distribution):
-	total = 0
-	for d in distribution:
-		total += d
-	part = fraction * total
-	index = 0
-	for d in distribution:
-		if part < d:
-			return index
-		index += 1
-		part -= d
-	return index
+    total = 0
+    for d in distribution:
+        total += d
+    part = fraction * total
+    index = 0
+    for d in distribution:
+        if part < d:
+            return index
+        index += 1
+        part -= d
+    return index
 
 
 def get_location(rnd):
-	return LOCATION[get_index(rnd.random(), [7, 1, 6, 8, 3, 2])]
+    return LOCATION[get_index(rnd.random(), [7, 1, 6, 8, 3, 2])]
 
 
 def get_tags(rnd):
-	samples = 1 + int(rnd.expovariate(1.5))
-	tags = rnd.sample(TAGS, samples)
+    samples = 1 + int(rnd.expovariate(1.5))
+    tags = rnd.sample(TAGS, samples)
 
-	# flatten nested lists
-	tags = [j for i in tags for j in i]
+    # flatten nested lists
+    tags = [j for i in tags for j in i]
 
-	# remove duplicates
-	return list(set(tags))
-	
-	
+    # remove duplicates
+    return list(set(tags))
+
+
 def get_profile(id, time_quantum):
-	profile = []
-	random.seed(id)
-	profile.extend(get_location(random))
-	random.seed(id * 1000000 + time_quantum)
-	profile.extend(get_tags(random))
-	return profile
-
+    profile = []
+    random.seed(id)
+    profile.extend(get_location(random))
+    random.seed(id * 1000000 + time_quantum)
+    profile.extend(get_tags(random))
+    return profile
--- hll.py
+++ hll.py
@@ -8,106 +8,99 @@ from aerospike_helpers.operations import hll_operations
 import generator
 
 
-parser = argparse.ArgumentParser(description='HLL example')
-parser.add_argument('--host', dest='host', default='127.0.0.1')
+parser = argparse.ArgumentParser(description="HLL example")
+parser.add_argument("--host", dest="host", default="127.0.0.1")
 args = parser.parse_args()
 
-HLL_BIN = 'hll_bin'
+HLL_BIN = "hll_bin"
 HLL_INDEX_BITS = 12
 HLL_MH_BITS = 0
 MONTHS = 12
 
 
-
-
 def init(host, port):
-	config = {
-		'hosts': [
-			( host, port ),
-		],
-		'policies': {
-			'timeout': 1000 # milliseconds
-		}
-	}
+    config = {
+        "hosts": [
+            (host, port),
+        ],
+        "policies": {"timeout": 1000},  # milliseconds
+    }
 
-	client = aerospike.client(config).connect()
-	return client
+    client = aerospike.client(config).connect()
+    return client
 
 
 def getkey(name, time):
-	return ('test', 'hll', '%s:%d' % (name, time))
+    return ("test", "hll", "%s:%d" % (name, time))
 
 
 # Ingest records with id in the range [start, end).
 def ingest_month(client, start, end, month):
-	# We don't need to initialise HLL bins; performing a HLL add to an empty bin will initialise it.
-	print 'Ingest ids %d-%d month %d' % (start, end, month)
+    # We don't need to initialise HLL bins; performing a HLL add to an empty bin will initialise it.
+    print "Ingest ids %d-%d month %d" % (start, end, month)
 
-	for i in range(start, end):
-		profile = generator.get_profile(i, month)
-		#print("Profile %d: %s" % (i, profile))
-		for tag in profile:
-			ops = [
-				hll_operations.hll_add(HLL_BIN, [str(i)], HLL_INDEX_BITS)
-			]
+    for i in range(start, end):
+        profile = generator.get_profile(i, month)
+        # print("Profile %d: %s" % (i, profile))
+        for tag in profile:
+            ops = [hll_operations.hll_add(HLL_BIN, [str(i)], HLL_INDEX_BITS)]
 
-			_, _, result = client.operate(getkey(tag, month), ops)
+            _, _, result = client.operate(getkey(tag, month), ops)
 
 
 def ingest(client):
-	for i in range(MONTHS):
-		ingest_month(client, 1, 20000, i)
+    for i in range(MONTHS):
+        ingest_month(client, 1, 20000, i)
 
 
 def count(client, tag, month):
-	ops = [
-		hll_operations.hll_get_count(HLL_BIN)
-	]
-	_, _, result = client.operate(getkey(tag, month), ops)
-	print 'tag:%s month:%d count:%d' % (tag, month, result[HLL_BIN])
-	#print result
+    ops = [hll_operations.hll_get_count(HLL_BIN)]
+    _, _, result = client.operate(getkey(tag, month), ops)
+    print "tag:%s month:%d count:%d" % (tag, month, result[HLL_BIN])
+    # print result
 
 
 def get_union_count(client, tag, t0, months):
-	times = range(t0+1, months)
-	records = [record[2][HLL_BIN] for record in client.get_many([getkey(tag, time) for time in times])]
+    times = range(t0 + 1, months)
+    records = [
+        record[2][HLL_BIN]
+        for record in client.get_many([getkey(tag, time) for time in times])
+    ]
 
-	ops = [
-		hll_operations.hll_get_union_count(HLL_BIN, records)
-	]
+    ops = [hll_operations.hll_get_union_count(HLL_BIN, records)]
 
-	_, _, result = client.operate(getkey(tag, t0), ops)
-	print 'tag:%s months:%d-%d count:%d' % (tag, t0, t0+months-1, result[HLL_BIN])
+    _, _, result = client.operate(getkey(tag, t0), ops)
+    print "tag:%s months:%d-%d count:%d" % (tag, t0, t0 + months - 1, result[HLL_BIN])
 
 
 def get_intersect_count(client, tags, month):
-	records = [record[2][HLL_BIN] for record in client.get_many([getkey(tag, month) for tag in tags[1:]])]
+    records = [
+        record[2][HLL_BIN]
+        for record in client.get_many([getkey(tag, month) for tag in tags[1:]])
+    ]
 
-	ops = [
-		hll_operations.hll_get_intersect_count(HLL_BIN, records)
-	]
+    ops = [hll_operations.hll_get_intersect_count(HLL_BIN, records)]
 
-	_, _, result = client.operate(getkey(tags[0], month), ops)
-	print 'tags:%s month:%d count:%d' % (tags, month, result[HLL_BIN])
+    _, _, result = client.operate(getkey(tags[0], month), ops)
+    print "tags:%s month:%d count:%d" % (tags, month, result[HLL_BIN])
 
 
 def main():
-	client = init(args.host, 3000)
-	ingest(client)
+    client = init(args.host, 3000)
+    ingest(client)
 
-	# Get counts
-	for i in range(MONTHS):
-		count(client, 'aerospike', i)
+    # Get counts
+    for i in range(MONTHS):
+        count(client, "aerospike", i)
 
-	# Get union
-	get_union_count(client, 'aerospike', 0, MONTHS)
+    # Get union
+    get_union_count(client, "aerospike", 0, MONTHS)
 
-
-	# Get intersection
-	count(client, 'vancouver', 0)
-	count(client, 'canada', 0)
-	count(client, 'washington', 0)
-	get_intersect_count(client, ['vancouver', 'washington'], 0)
+    # Get intersection
+    count(client, "vancouver", 0)
+    count(client, "canada", 0)
+    count(client, "washington", 0)
+    get_intersect_count(client, ["vancouver", "washington"], 0)
 
 
 main()
